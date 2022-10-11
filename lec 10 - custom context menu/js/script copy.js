96f8523e7c60cb9e26863bb1dbc8c08aede76034
Ahmed Alghoul <Ahmed.Alghoul98@gmail.com>
2022-10-12 01:49:19 +0300

    commented unnecessary code

--- lec 10 - custom context menu/js/script copy.js
+++ lec 10 - custom context menu/js/script copy.js
@@ -1,22 +1,22 @@
 const contextMenu = document.querySelector(".context-menu");
 const scope = document.querySelector("body");
 
-scope.addEventListener("contextmenu", (event) => {
-    event.preventDefault();
-    const { clientX: mouseX, clientY: mouseY } = event;
-    contextMenu.style.top = `${mouseY}px`;
-    contextMenu.style.left = `${mouseX}px`;
-    contextMenu.classList.remove("visible");
-    setTimeout(() => {
-        contextMenu.classList.add("visible");
-    });
+// scope.addEventListener("contextmenu", (event) => {
+//     event.preventDefault();
+//     const { clientX: mouseX, clientY: mouseY } = event;
+//     contextMenu.style.top = `${mouseY}px`;
+//     contextMenu.style.left = `${mouseX}px`;
+//     contextMenu.classList.remove("visible");
+//     setTimeout(() => {
+//         contextMenu.classList.add("visible");
+//     });
 
-});
-scope.addEventListener("click", (event) => {
-    if (event.target != contextMenu) {
-        contextMenu.classList.remove("visible");
-    }
-});
+// });
+// scope.addEventListener("click", (event) => {
+//     if (event.target != contextMenu) {
+//         contextMenu.classList.remove("visible");
+//     }
+// });
 
 //prevent menu from going out of bounds of the screen
 const normalizePozition = (mouseX, mouseY) => {
